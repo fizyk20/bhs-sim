05abb39f607e854403d122af2908586cf6593e34
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-02-15 17:14:39 +0100

    Added flat calculation for comparison

--- deflection-table-gen/gen_table.cpp
+++ deflection-table-gen/gen_table.cpp
@@ -22,45 +22,60 @@ using namespace std;
 const double M = 0.5;
 SchwManifold manifold(M);
 
-vector4 ray(double r, double phi)
+vector4 ray(double M, double r, double phi)
 {
 	vector4 T(-1.0/sqrt(2), (M/r + 0.5)/sqrt(2), 0., 0.);
 	vector4 A(-1.0/sqrt(2), (M/r - 1.5)/sqrt(2), 0., 0.);
 
-	return T + A*cos(phi) + vector4(0., 0., 0., 1./r)*sin(phi);
+	return T + A*cos(phi) + vector4(0., 0., 0., -1./r)*sin(phi);
 }
 
-double deflection(double r, double phi)
+double deflected_final_phi(double r, double phi)
 {
-	Particle photon(&manifold, Point(EF, 0.0, r, M_PI/2, -M_PI), ray(r, phi));
+	Particle photon(&manifold, Point(EF, 0.0, r, M_PI/2, M_PI), ray(M, r, phi));
 	DPIntegrator integrator;
 	integrator.setStepSize(1e-4);
 	integrator.setMaxErr(1e-12);
 	photon.setIntegrator(&integrator);
 
 	Metric* g = manifold.getMetric(EF);
-	while(photon.getPos()[1] < 200.0*M && photon.getPos()[1] > HORIZON)
+	while(photon.getPos()[1] < 1000.0*M && photon.getPos()[1] > HORIZON)
 	{
-		cout << "\tr = " << photon.getPos()[1] << "\t" << g->g(photon.getVel(), photon.getVel(), photon.getPos()) << endl;
+		//cout << "\tr = " << photon.getPos()[1] << "\tphi = " << photon.getPos()[3] << "\t" << g->g(photon.getVel(), photon.getVel(), photon.getPos()) << endl;
 		photon.propagate();
 	}
 
 	if(photon.getPos()[1] <= HORIZON)
 		return -1000.0;
 
-	return photon.getPos()[3];	// phi coordinate
+	double final_phi = photon.getPos()[3];	// phi coordinate
+
+	double factor = sqrt(1.0 - 2.0*M/photon.getPos()[1]);
+
+	// approximate the rest of the path by a straight line - calculate the additional angle
+	double vr = factor*photon.getVel()[0] + photon.getVel()[1]/factor;
+	double vphi = photon.getPos()[1] * photon.getVel()[3];
+
+	double add_phi = atan2(vphi, vr);
+
+	return final_phi + add_phi;
+}
+
+double flat_final_phi(double phi)
+{
+	double cosphi = cos(phi);
+	return acos((3*cosphi-1)/(3-cosphi));
 }
 
 int main()
 {
-	double r = 3.0;
+	double r = 20.0;
 
 	for(int i = 0; i < N_POINTS + 1; i++)
 	{
 		double phi = M_PI * i / N_POINTS;
-		double result = deflection(r, phi);
-		if(result != -1000.0) result = -(result + phi);
-		cout << "Phi = " << phi << "\t Deflection = " << result << endl;
+		double result = deflected_final_phi(r, phi);
+		cout << "Phi = " << phi << "\tFinal phi = " << result << "\tFlat final phi = " << flat_final_phi(phi) << endl;
 	}
 	return 0;
 }
